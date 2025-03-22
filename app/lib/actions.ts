6c49860d35d644ebbc0b63da3faf385f88d14595
= <aguibw@gmail.com>
2025-03-22 17:19:28 +0000

    fixed invoices delete action

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -109,8 +109,6 @@ export async function updateInvoice(
 }
 
 export async function deleteInvoice(id: string) {
-  throw new Error("Failed to Delete Invoice");
-
   await sql`DELETE FROM invoices WHERE id = ${id}`;
   revalidatePath("/dashboard/invoices");
 }
